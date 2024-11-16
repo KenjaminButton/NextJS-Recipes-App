50fd7c757f64a79b9e3524f71eb3a5f308418bb2
Kenjamin Button <kenjaminbutton@mac.lan>
2024-11-16 12:47:34 -0800

    Share meal page form rendered

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Filmic Pro Recipe Not Found</h1>
+      <p>This error is on your part... Sounds like a you problem, not a me problem 😉</p>
+    </main>
+  )
+}
