8b91692e5e72695609c5c383481f4c5a204dc542
Kenjamin Button <kenjaminbutton@mac.lan>
2024-11-15 22:58:15 -0800

    recipe and recipe item grid components set up

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,25 @@
+import MealsGrid from '@/components/main-header/meals/meals-grid'
+import classes from './page.module.css'
+import Link from 'next/link'
+
 export default function MealsPage() {
   return (
-    <h1>Meals Page</h1>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious filters, created {' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite Fuji film recipes.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   )
 }
